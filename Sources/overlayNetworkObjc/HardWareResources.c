cda8bef32dde8feb83099d16bf58b23cf134a652
webbananaunite <mizu@webbanana.org>
2024-01-11 18:17:58 +0900

    Make Notify to user for have joined network.

---
+++ Sources/overlayNetworkObjc/HardWareResources.c
@@ -0,0 +1,8 @@
+//
+//  HardWareResources.c
+//  
+//
+//  Created by よういち on 2024/01/06.
+//
+
+#include "HardWareResources.h"
---
+++ Sources/overlayNetworkObjc/headers/HardWareResources.h
@@ -0,0 +1,13 @@
+//
+//  HardWareResources.h
+//  
+//
+//  Created by よういち on 2024/01/06.
+//
+
+#ifndef HardWareResources_h
+#define HardWareResources_h
+
+#include <stdio.h>
+
+#endif /* HardWareResources_h */
